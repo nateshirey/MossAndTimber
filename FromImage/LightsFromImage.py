e622a4793238cd081bcaa1172087128c95b8a9e4
Nate Shirey <nateshirey6@gmail.com>
2020-12-01 20:30:11 -0700

    image light patterns working

--- FromImage/LightsFromImage.py
+++ FromImage/LightsFromImage.py
@@ -43,29 +43,72 @@ row7.reverse()
 ##make a new array that has all lights' colors
 ##and add each row to it
 allLights = []
+allReds = []
+allGreens = []
+allBlues = []
+
 for i in range(len(row1)):
     allLights.append(row1[i])
+    allReds.append(row1[i][0])
+    allGreens.append(row1[i][1])
+    allBlues.append(row1[i][2])
 for i in range(len(row2)):
     allLights.append(row2[i])
+    allReds.append(row2[i][0])
+    allGreens.append(row2[i][1])
+    allBlues.append(row2[i][2])
 for i in range(len(row3)):
     allLights.append(row3[i])
+    allReds.append(row3[i][0])
+    allGreens.append(row3[i][1])
+    allBlues.append(row3[i][2])
 for i in range(len(row4)):
     allLights.append(row4[i])
+    allReds.append(row4[i][0])
+    allGreens.append(row4[i][1])
+    allBlues.append(row4[i][2])
 for i in range(len(row5)):
     allLights.append(row5[i])
+    allReds.append(row5[i][0])
+    allGreens.append(row5[i][1])
+    allBlues.append(row5[i][2])
 for i in range(len(row6)):
     allLights.append(row6[i])
+    allReds.append(row6[i][0])
+    allGreens.append(row6[i][1])
+    allBlues.append(row6[i][2])
 for i in range(len(row7)):
     allLights.append(row7[i])
+    allReds.append(row7[i][0])
+    allGreens.append(row7[i][1])
+    allBlues.append(row7[i][2])
 for i in range(len(row8)):
     allLights.append(row8[i])
+    allReds.append(row8[i][0])
+    allGreens.append(row8[i][1])
+    allBlues.append(row8[i][2])
     
 ##open the output file as writable to close it as blank
 open('outputFile.txt', 'w').close()
+open('outputFileReds.txt', 'w').close()
+open('outputFileGreens.txt', 'w').close()
+open('outputFileBlues.txt', 'w').close()
 
 ##open the now blank output file and populate it with light colors
 outputFile = open('outputFile.txt', 'a')
+outputFileReds = open('outputFileReds.txt', 'a')
+outputFileGreens = open('outputFileGreens.txt', 'a')
+outputFileBlues = open('outputFileBlues.txt', 'a')
+
 outputFile.write(str(allLights))
+outputFileReds.write(str(allReds))
+outputFileGreens.write(str(allGreens))
+outputFileBlues.write(str(allBlues))
+
 outputFile.close()
+outputFileReds.close()
+outputFileGreens.close()
+outputFileBlues.close()
+
 
 
