66dd7a9ee7d9fb1aab34059ef708236d13db8898
Nate Shirey <nateshirey6@gmail.com>
2020-12-02 16:10:14 -0700

    testing writing to the image file

--- FromImage/LightsFromImage.py
+++ FromImage/LightsFromImage.py
@@ -42,73 +42,114 @@ row7.reverse()
 
 ##make a new array that has all lights' colors
 ##and add each row to it
-allLights = []
 allReds = []
 allGreens = []
 allBlues = []
 
 for i in range(len(row1)):
-    allLights.append(row1[i])
     allReds.append(row1[i][0])
     allGreens.append(row1[i][1])
     allBlues.append(row1[i][2])
 for i in range(len(row2)):
-    allLights.append(row2[i])
     allReds.append(row2[i][0])
     allGreens.append(row2[i][1])
     allBlues.append(row2[i][2])
 for i in range(len(row3)):
-    allLights.append(row3[i])
     allReds.append(row3[i][0])
     allGreens.append(row3[i][1])
     allBlues.append(row3[i][2])
 for i in range(len(row4)):
-    allLights.append(row4[i])
     allReds.append(row4[i][0])
     allGreens.append(row4[i][1])
     allBlues.append(row4[i][2])
 for i in range(len(row5)):
-    allLights.append(row5[i])
     allReds.append(row5[i][0])
     allGreens.append(row5[i][1])
     allBlues.append(row5[i][2])
 for i in range(len(row6)):
-    allLights.append(row6[i])
     allReds.append(row6[i][0])
     allGreens.append(row6[i][1])
     allBlues.append(row6[i][2])
 for i in range(len(row7)):
-    allLights.append(row7[i])
     allReds.append(row7[i][0])
     allGreens.append(row7[i][1])
     allBlues.append(row7[i][2])
 for i in range(len(row8)):
-    allLights.append(row8[i])
     allReds.append(row8[i][0])
     allGreens.append(row8[i][1])
     allBlues.append(row8[i][2])
-    
-##open the output file as writable to close it as blank
-open('outputFile.txt', 'w').close()
-open('outputFileReds.txt', 'w').close()
-open('outputFileGreens.txt', 'w').close()
-open('outputFileBlues.txt', 'w').close()
 
-##open the now blank output file and populate it with light colors
-outputFile = open('outputFile.txt', 'a')
-outputFileReds = open('outputFileReds.txt', 'a')
-outputFileGreens = open('outputFileGreens.txt', 'a')
-outputFileBlues = open('outputFileBlues.txt', 'a')
 
-outputFile.write(str(allLights))
-outputFileReds.write(str(allReds))
-outputFileGreens.write(str(allGreens))
-outputFileBlues.write(str(allBlues))
 
-outputFile.close()
-outputFileReds.close()
-outputFileGreens.close()
-outputFileBlues.close()
+
+allRedsString = 'uint8_t reds [] = {'
+for i in range(len(allReds) - 1):
+    allRedsString = allRedsString + str(allReds[i]) + ','
+
+allRedsString = allRedsString + str(allReds[len(allReds) - 1]) + '};\n'
+
+allGreensString = 'uint8_t greens [] = {'
+for i in range(len(allGreens) - 1):
+    allGreensString = allGreensString + str(allGreens[i]) + ','
+
+allGreensString = allGreensString + str(allGreens[len(allGreens) - 1]) + '};\n'
+
+allBluesString = 'uint8_t blues [] = {'
+for i in range(len(allBlues) - 1):
+    allBluesString = allBluesString + str(allBlues[i]) + ','
+
+allBluesString = allBluesString + str(allBlues[len(allBlues) - 1]) + '};\n'
+
+open('tempFile.txt', 'w').close()
+
+file = open('FromImage.ino')
+temp = open('tempFile.txt', 'w')
+
+lineNumber = 0
+foundStartFlag = False
+foundEndFlag = False
+
+for line in file:
+    lineNumber += 1
+    if foundStartFlag:
+        break
+    if '//paste colors here' in line:
+        print(lineNumber)
+        foundStartFlag = True
+    temp.write(line)
+
+temp.write(allRedsString)
+temp.write(allGreensString)
+temp.write(allBluesString)
+temp.write('\n')
+lineNumber += 4
+
+for line in file:
+    if foundEndFlag:
+        temp.write(line)
+    if '//end paste block' in line:
+        foundEndFlag = True
+
+
+
+##for line in file:
+##    lineNumber -= 1
+##    if lineNumber > 0:
+##        pass
+##    temp.write(line)
+
+temp.close()
+file.close()
+
+file = open('FromImage.ino', 'w')
+temp = open('tempFile.txt')
+
+for line in temp:
+    file.write(line)
+
+file.close()
+temp.close()
+
 
 
 
