a62dd6ff10a1a6eaa4c3c762957ab7658c7a387a
Nate Shirey <nateshirey6@gmail.com>
2021-06-09 18:17:24 -0600

    mostly done with fireworks

--- FireWorks/FireWorks/FireWorks.ino
+++ FireWorks/FireWorks/FireWorks.ino
@@ -12,6 +12,8 @@
 
 //paste colors here
 uint16_t fireWorks [4][46]{};
+//fireWorks[x][y] are x number of arrays that contain y number of lights
+//the following arrays are stages of a firework animation
 uint8_t fireWorkAnimRed[] = {255, 223, 191, 159, 127, 95, 63, 31};
 uint8_t fireWorkAnimGreen[] = {255, 150, 0, 0, 0, 0, 0, 0};
 uint8_t fireWorkAnimBlue[] = {255, 150, 20, 20, 0, 0, 0, 0};
@@ -27,7 +29,10 @@ Adafruit_NeoPixel pixels(lightsNum, PIN, NEO_GRB + NEO_KHZ800);
 
 long Time = 0;
 long Speed = 10;
+//current firework is the index of the next firework to be instantiated
 uint8_t currentFireWork = 0;
+//randLight is 8 random numbers in the range of the firework array used for sparkling
+uint8_t randLight[8] = {};
 
 void setup() {
 
@@ -37,19 +42,26 @@ void setup() {
   Serial.begin(9600);
 }
 
+//this function populates the oldest firework array with new light numbers
 void MakeFireworkArray(uint16_t center)
 {   
+  //The first 8 indexes are the 8 lights forward in the strip
     fireWorks[currentFireWork][0] = center;
     for(int i=1; i<9; i++){
       fireWorks[currentFireWork][i] = center + i;
     }
+    //the next 8 are the 8 lights backwards in the strip
     for(int i=1; i<9; i++){
       fireWorks[currentFireWork][i+8] = center - i;
     }
+    //not sure what this was
     for(int i=17; i<29; i++){
-      fireWorks[currentFireWork][i] = 10;
+      fireWorks[currentFireWork][i] = 0;
     }
+    //the 29th index is used for the timer on the firework
     fireWorks[currentFireWork][29] = 255;
+    //this next part gets the indeces of the lights directly up and down 
+    //depending on where the center of the light is
     //row 1
     if(center < 134){
       for(int i=30; i<38;i++){
@@ -117,7 +129,8 @@ void MakeFireworkArray(uint16_t center)
       }
     }
 }
-
+//this just sets the index of the light to 0 for each light after the timer
+//runs out
 void KillFirework(uint8_t index){
   for(int j = 0; j<45; j++){
     fireWorks[index][j] = 0;
@@ -126,7 +139,7 @@ void KillFirework(uint8_t index){
 
 void loop() {
   Time = Time + Speed;
-
+  //count up by the speed and if the timer hits 255, make a new firework
   if(Time > 255) {
     Time = 0;
     currentFireWork++;
@@ -139,21 +152,30 @@ void loop() {
       Serial.println(fireWorks[currentFireWork][i]);
     }
   }
-  uint32_t off = pixels.Color(0, 50, 100);
+  //set all the pixels to the background color
+  uint32_t back = pixels.Color(0, 40, 120);
   for(int i = 0; i < lightsNum; i++){
-    pixels.setPixelColor(i, off);
+    pixels.setPixelColor(i, back);
+  }
+
+  //pick 8 random lights to flicker
+  for(int r=0; r<7; r++){
+    randLight[r] = random(0,45);
   }
   
-  for(int i=0; i<3; i++) { // For each pixel...
+  for(int i=0; i<3; i++) { // For each firework array...
+    //clamp the timer
     if(fireWorks[i][29] > 255){
       fireWorks[i][29] = 255;
     }
-    fireWorks[i][29] = fireWorks[i][29] - Speed/2;
-    //less than 32
+    //count the timer down
+    fireWorks[i][29] = fireWorks[i][29] - Speed;
+    //if the timer is still close to the beginning...
     uint32_t fireWorkColor = pixels.Color(fireWorkAnimRed[0], fireWorkAnimGreen[0], fireWorkAnimBlue[0]);
     if(fireWorks[i][29] < 210){
       fireWorkColor = pixels.Color(fireWorkAnimRed[1], fireWorkAnimGreen[1], fireWorkAnimBlue[1]);
     }
+    //if the timer is in the 160 - 192 range...
     if(fireWorks[i][29] < 192){
       fireWorkColor = pixels.Color(fireWorkAnimRed[2], fireWorkAnimGreen[2], fireWorkAnimBlue[2]);
     }
@@ -172,14 +194,24 @@ void loop() {
     if(fireWorks[i][29] < 32){
       fireWorkColor = pixels.Color(fireWorkAnimRed[7], fireWorkAnimGreen[7], fireWorkAnimBlue[7]);
     }
+    //if the timer gets close to 0, kill
     if(fireWorks[i][29] < 10){
       KillFirework(i);
     }
+    //for each pixel in the firework
     for(int j=0; j<46; j++){
+      //set the pixel color , dont think i should be setting the timer
       pixels.setPixelColor(fireWorks[i][j], fireWorkColor);
+      uint32_t flicker = pixels.Color(255, 232, 36);
+      //then for each light, check if it is one of the flicker values and set it
+      for(int r=0; r<2; r++){
+        if(randLight[r] != 29){
+          if(j == randLight[r]){
+            pixels.setPixelColor(fireWorks[i][j], flicker);
+          }
+        }
+      }
     }
-    uint32_t white = pixels.Color(255, 255, 255);
-    pixels.setPixelColor(fireWorks[i][0], white);
   }
     
   pixels.show();   // Send the updated pixel colors to the hardware.
